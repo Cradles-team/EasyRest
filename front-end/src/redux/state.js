736e8d26ef829c83db675f1819ef8d6f274aa677
mykyta.vyzhol <flamenv22@gmail.com>
2019-11-28 19:08:38 +0200

    fix code lines

--- front-end/src/redux/state.js
+++ front-end/src/redux/state.js
@@ -3,29 +3,49 @@ let state = {
         {id: 1, name: "Lviv Croissants", image: "https://lviv.virtual.ua/images/387175/"},
         {id: 2, name: "Celentano", image: "https://f.zaporizhzhia.city/location/2781/GzqK6.jpeg"},
         {id: 3, name: "Кебаб Шеф", image: "https://media-cdn.tripadvisor.com/media/photo-s/12/53/18/97/caption.jpg"},
-        {id: 4, name: "Aroma Kava", image: "https://tomato.ua/image/resize/storage/restaurants/5a3914bbf88a8b003e55904b/15136902995a3914bb3c805_5a3914b5ebe7c6.23782622.jpeg?w=1200&h=1200"},
-        {id: 5, name: "Soda Buffet", image: "https://s3.eu-central-1.amazonaws.com/listmusor/production/187257/gallery/big/5d256c266d7af.jpg?1316609607"},
+        {
+            id: 4,
+            name: "Aroma Kava",
+            image: "https://tomato.ua/image/resize/storage/restaurants/5a3914bbf88a8b003e55904b/15136902995a3914bb3c805_5a3914b5ebe7c6.23782622.jpeg?w=1200&h=1200"
+        },
+        {
+            id: 5,
+            name: "Soda Buffet",
+            image: "https://s3.eu-central-1.amazonaws.com/listmusor/production/187257/gallery/big/5d256c266d7af.jpg?1316609607"
+        },
         {id: 6, name: "Інтемпо", image: "https://lviv.virtual.ua/images/415143/"}],
 
     restsInfo: [
-        {id: 1,
+        {
+            id: 1,
             photo: "http://franchithink.com/wp-content/uploads/2018/06/Lvivski-kruasani05593-1160x570.jpg",
-                description: "Lviv Croissants - найсмачніші круасани у Львові"},
-        {id: 2,
+            description: "Lviv Croissants - найсмачніші круасани у Львові"
+        },
+        {
+            id: 2,
             photo: "https://franch.biz/franch/file/1935/pizza-celentano-2720663.jpg",
-                description: "Celentano - піца на будь-який смак"},
-        {id: 3,
+            description: "Celentano - піца на будь-який смак"
+        },
+        {
+            id: 3,
             photo: "https://tomato.ua/image/resize/storage/restaurants//15349138775b7ced5541be1_5T8YhnfpCLnN9kI5dXImap4QEpmIWLvwcLDOq15V.jpeg?w=1200&h=1200",
-            description: "Кебаб Шеф - їжа для справжніх чоловіків"},
-        {id: 4,
+            description: "Кебаб Шеф - їжа для справжніх чоловіків"
+        },
+        {
+            id: 4,
             photo: "https://rau.ua/wp-content/uploads/2018/12/Conc2.jpg",
-            description: "Aroma Kava - найкраща кава у зимовий день"},
-        {id: 5,
+            description: "Aroma Kava - найкраща кава у зимовий день"
+        },
+        {
+            id: 5,
             photo: "https://s3.eu-central-1.amazonaws.com/listmusor/production/187257/gallery/big/5d256d4964f36.jpg?1316609607",
-            description: "Soda Buffet - дешево та смачно"},
-        {id: 6,
+            description: "Soda Buffet - дешево та смачно"
+        },
+        {
+            id: 6,
             photo: "https://lviv.virtual.ua/images/415152/",
-            description: "Інтемпо - сітка ресторанів у Львові"}
+            description: "Інтемпо - сітка ресторанів у Львові"
+        }
     ]
 }
 
