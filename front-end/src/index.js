ad8b460bace82105134f9f8a5460cd0aa683b365
mykyta.vyzhol <flamenv22@gmail.com>
2020-03-17 16:56:35 +0200

    continued to introduce flux architecture (react redux) with OOP principles

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,22 @@
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from "./redux/render";
-import state from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+import store from "./redux/state";
 
-rerenderEntireTree(state);
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={store.getState()}/>
+        </BrowserRouter>, document.getElementById('root'));
+};
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
