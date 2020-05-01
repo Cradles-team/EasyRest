c924ba3b7b1f7832de289a31f87de7dbe9178001
Vadym <vadijjolenuch@gmail.com>
2020-05-02 00:22:24 +0300

    added folder "design" and code for new sensors

--- sensors/sensor/sensor.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  Serial.begin(2400);
-  Serial.print("Hello");
-  pinMode(2, INPUT);
-}
-void loop() {
-  boolean sensor1 = digitalRead(2);
-  Serial.print("Sensor1: ");Serial.println(sensor1);
- }
